bdda517f2cd430e3da4d69b553a4f6c013304cb1
NeilaVait <79705047+NeilaVait@users.noreply.github.com>
2021-06-22 09:46:26 +0300

    form + style

--- src/components/mainForm.jsx
+++ src/components/mainForm.jsx
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
 
 class MainForm extends Component {
-  state = {};
+  state = {
+    account: {},
+  };
+
+  handleForm(event) {
+    event.preventDefault();
+  }
+
   render() {
     return (
       <div className="main-form">
         <h1>Main form</h1>
+        <form onSubmit={this.handleForm}>
+          <input type="text" autoComplete="off" name="username" placeholder="Username" />
+          <input type="text" autoComplete="off" name="email" placeholder="Email" />
+          <input type="password" name="password" placeholder="Password" />
+          <input type="password" name="reapeatPassword" placeholder="Repeat password" />
+          <div>
+            <label htmlFor="agreement">I agree</label>
+            <input id="checkbox" name="checkbox" type="checkbox"></input>
+            <button type="submit">Send</button>
+          </div>
+        </form>
       </div>
     );
   }
